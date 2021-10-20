91f9e5b8ce22ae27c30d340f88fd532a8dec1d8f
Rafael Viana <rvlaraujo@gmail.com>
2021-10-20 01:02:03 -0300

    Creating the Artist controller with the index action and its views, helpers and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'artists', to: 'artists#index', as: 'artists'
   devise_for :users
   resources :albums
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
--- spec/system/login_flow_spec.rb
+++ spec/system/login_flow_spec.rb
@@ -3,7 +3,8 @@ require 'rails_helper'
 RSpec.describe 'Login Flow Features', type: :system do
   it 'If a not-logged in user tries to access any internal page he should be redirect to the login page' do
     visit albums_path
-
+    expect(page).to have_current_path(new_user_session_path)
+    visit artists_path
     expect(page).to have_current_path(new_user_session_path)
   end
 end
