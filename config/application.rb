d7f987854d2987e4b04b325a2575458aab91f15b
Rafael Viana <rvlaraujo@gmail.com>
2021-10-24 19:10:21 -0300

    Custom Devise Layout and styling the login page

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,14 @@ module RailsMusicCollectionApp
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    # Devise Custom layout
+    config.to_prepare do
+      Devise::SessionsController.layout "devise"
+      Devise::RegistrationsController.layout proc{ |controller| user_signed_in? ? "application" : "devise" }
+      Devise::ConfirmationsController.layout "devise"
+      Devise::UnlocksController.layout "devise"            
+      Devise::PasswordsController.layout "devise"        
+    end
   end
 end
