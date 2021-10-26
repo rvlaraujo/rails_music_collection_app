cb2306adb8680ccc44c7abab759197a656c9fca5
Rafael Viana <rvlaraujo@gmail.com>
2021-10-26 18:44:38 -0300

    New style for Application notice message

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,15 @@ module ApplicationHelper
     client = MoatClient.new
     client.find_artist(artist_id).name
   end
+
+  def flash_class(level)
+    bootstrap_alert_class = {
+      'success' => 'alert-info',
+      'error' => 'alert-danger',
+      'notice' => 'alert-success',
+      'alert' => 'alert-warning',
+      'warn' => 'alert-warning'
+    }
+    bootstrap_alert_class[level]
+  end
 end
