b9c6f605e36513b29b52b350badc441f525cf1c2
Rafael Viana <rvlaraujo@gmail.com>
2021-10-27 10:06:37 -0300

    Fix Registration User validations

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,16 +5,19 @@ RSpec.describe User, type: :model do
     it { should validate_presence_of(:username) }
     it { should validate_length_of(:username).is_at_least(6) }
     it { should validate_uniqueness_of(:username).case_insensitive }
-    pending 'username regex'
   end
 
   context 'Full Name validations' do
     it { should validate_presence_of(:full_name) }
     it { should validate_length_of(:full_name).is_at_least(8) }
-    pending 'full name regex not permited numbers'
   end
 
   context 'Role validations' do
     it { should define_enum_for(:role).with_values(%i[user admin]) }
+  end
+
+  context 'Password Validatations' do
+    it { should validate_presence_of(:password) }
+    it { should validate_length_of(:password).is_at_least(6) }
   end
 end
---
+++ spec/system/registration_user_spec.rb
@@ -0,0 +1,89 @@
+require 'rails_helper'
+require 'faker'
+
+RSpec.describe 'Registration Users', type: :system do
+
+  it 'Registration a user with blank username' do
+    visit new_user_registration_path
+    fill_in 'Username', with: ''
+    fill_in 'Full name', with: Faker::FunnyName.two_word_name
+    fill_in 'Password', with: '123456'
+    fill_in 'Password confirmation', with: '123456'
+
+    click_on 'Sign up'
+
+    expect(page).to have_content("Username can't be blank")
+  end
+
+  it 'Registration a user with invalid username' do
+    visit new_user_registration_path
+    fill_in 'Username', with: Faker::Name.initials(number: 5)
+    fill_in 'Full name', with: Faker::FunnyName.two_word_name
+    fill_in 'Password', with: '123456'
+    fill_in 'Password confirmation', with: '123456'
+
+    click_on 'Sign up'
+
+    expect(page).to have_content('Username is too short (minimum is 6 characters)')
+  end
+
+  it 'Registration a user with blank Full name' do
+    visit new_user_registration_path
+    fill_in 'Username', with: Faker::Name.initials(number: 6)
+    fill_in 'Full name', with: ''
+    fill_in 'Password', with: '123456'
+    fill_in 'Password confirmation', with: '123456'
+
+    click_on 'Sign up'
+
+    expect(page).to have_content("Full name can't be blank")
+  end
+
+  it 'Registration a user with invalid Full name' do
+    visit new_user_registration_path
+    fill_in 'Username', with: Faker::Name.initials(number: 6)
+    fill_in 'Full name', with: Faker::Name.initials(number: 7)
+    fill_in 'Password', with: '123456'
+    fill_in 'Password confirmation', with: '123456'
+
+    click_on 'Sign up'
+
+    expect(page).to have_content("Full name is too short (minimum is 8 characters)")
+  end
+
+  it 'Registration a user with blank passowrd' do
+    visit new_user_registration_path
+    fill_in 'Username', with: Faker::Name.initials(number: 6)
+    fill_in 'Full name', with: Faker::FunnyName.two_word_name
+    fill_in 'Password', with: ''
+    fill_in 'Password confirmation', with: ''
+
+    click_on 'Sign up'
+
+    expect(page).to have_content("Password can't be blank")
+  end
+
+  it 'Registration a user with invalid password' do
+    visit new_user_registration_path
+    fill_in 'Username', with: Faker::Name.initials(number: 6)
+    fill_in 'Full name', with: Faker::FunnyName.two_word_name
+    fill_in 'Password', with: '12345'
+    fill_in 'Password confirmation', with: '12345'
+
+    click_on 'Sign up'
+
+    expect(page).to have_content("Password is too short (minimum is 6 characters)")
+  end
+
+  it 'Try to registrate a valid user' do
+    visit new_user_registration_path
+    fill_in 'Username', with: Faker::Name.initials(number: 6)
+    fill_in 'Full name', with: Faker::FunnyName.two_word_name
+    fill_in 'Password', with: '123456'
+    fill_in 'Password confirmation', with: '123456'
+
+    click_on 'Sign up'
+
+    expect(page).to have_current_path(artists_path)
+  end
+end
