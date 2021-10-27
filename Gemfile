df79437c50cac7d1287f33f7cd9f542dad79bff1
Rafael Viana <rvlaraujo@gmail.com>
2021-10-27 01:43:29 -0300

    Pagination implemented with kaminari

--- Gemfile
+++ Gemfile
@@ -34,6 +34,9 @@ gem 'bootsnap', '>= 1.4.4', require: false
 # Devise for authentication
 gem 'devise'
 
+# Kaminari for pagination
+gem 'kaminari'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: %i[mri mingw x64_mingw]
--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -2,12 +2,12 @@ class ArtistsController < ApplicationController
   before_action :set_moat_client
 
   def index
-    @artists = @client.all_artists
+    @artists = Kaminari.paginate_array(@client.all_artists).page(params[:page]).per(15)
   end
 
   def show
     @artist = @client.find_artist(params[:id])
-    @albums = Album.where({artist_id: @artist.id})
+    @albums = Album.where({ artist_id: @artist.id })
   end
 
   private
