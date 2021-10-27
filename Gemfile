ac3e327dde75fecd3f99fa4cf0bc9e8179a07a6a
Rafael Viana <rvlaraujo@gmail.com>
2021-10-27 00:06:29 -0300

    Adding seed to populating the system with some fake data

--- Gemfile
+++ Gemfile
@@ -37,6 +37,7 @@ gem 'devise'
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: %i[mri mingw x64_mingw]
+  gem 'faker', git: 'https://github.com/faker-ruby/faker.git', branch: 'master'
   gem 'rspec-rails', '~> 5.0.0'
 end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,22 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+def create_user(user)
+  if User.find_by(username: user.username).nil?
+    user.save!
+  end
+end
+
+create_user(User.new(username: 'administrator', full_name: 'Administrator', password: 'administrator', password_confirmation: 'administrator',
+                     role: User.roles[:admin]))
+create_user(User.new(username: 'commonuser', full_name: 'Common User', password: 'commonuser', password_confirmation: 'commonuser',
+                     role: User.roles[:user]))
+
+## Setting up the Artists List
+client = MoatClient.new
+artists = client.all_artists
+
+## creating albums
+(1..35).each do |_e|
+  Album.find_or_create_by!(artist_id: artists.sample.id, name: Faker::Music.album,
+                           year: Faker::Number.between(from: 1980, to: 2021))
+end
